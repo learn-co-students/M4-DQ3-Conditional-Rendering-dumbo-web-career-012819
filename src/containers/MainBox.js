f93d23079a0cf59bab69bff0fa21e24ce174631a
Maximo Bautista <max.bautista97@gmail.com>
2019-04-04 10:52:25 -0400

    Done.

--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -5,19 +5,49 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 class MainBox extends React.Component {
 
 
+  state = {
+    display: "profile"
+  }
+
+  switchState = (newState) => {
+      document.querySelector('.item.active').className = "item";
+      document.querySelector(`#${newState}`).className = "item active";
+      this.setState({display:newState})
+  }
+
+  componentDidMount(){
+    document.querySelector(`#${this.state.display}`).className = "item active";
+  }
+
+  chooseView () {
+    if(this.state.display === "profile") {
+      return <Profile />
+    } else if(this.state.display === "photo") {
+      return <Photos />
+    } else if(this.state.display === "cocktail") {
+      return <Cocktails />
+    } else if(this.state.display === "pokemon") {
+      return <Pokemon />
+    }
+  }
+
   render() {
 
     /*
 
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
+      Replace the code below! Depending on what menu item is selected in the menu,
+      I should render either a Profile, Photos, Cocktails, or Pokemon component.
+      Think of a way to track which menu item was selected.
+      Which component should have state? Which component should have methods to control state?
+      Where should these methods be called?
 
     */
+    let detailsToDisplay = this.chooseView();
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
 
     return (
       <div>
-        <MenuBar />
+        <MenuBar selected={this.state.display} stateFunction={this.switchState} />
         {detailsToDisplay}
       </div>
     )
